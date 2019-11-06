5537e7a6c1c0148fc1b1b4c2920dab54c2147b91
Rupert Smith <rupert@thesett.com>
2019-11-06 16:02:45 +0000

    Implemented guarded types.

--- src/Guarded.elm
+++ src/Guarded.elm
@@ -1,22 +1,43 @@
 module Guarded exposing (..)
 
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode exposing (Value)
+import Regex
+
 
 type Guarded i a e
-    = Guarded (i -> Result e a) (e -> String) (a -> i)
+    = Guarded (i -> Result e a) (Decoder i) (i -> Value) (e -> String) (a -> i)
 
 
-make : (i -> Result e a) -> (e -> String) -> (a -> i) -> Guarded
+make : (i -> Result e a) -> Decoder i -> (i -> Value) -> (e -> String) -> (a -> i) -> Guarded i a e
 make =
     Guarded
 
 
 build : Guarded i a e -> i -> Result e a
+build (Guarded buildFn _ _ _ _) val =
+    buildFn val
 
 
 decoder : Guarded i a e -> Decoder a
+decoder (Guarded buildFn decoderI _ errorToStringFn _) =
+    decoderI
+        |> Decode.andThen
+            (\val ->
+                case buildFn val of
+                    Ok value ->
+                        Decode.succeed value
+
+                    Err err ->
+                        errorToStringFn err
+                            |> Decode.fail
+            )
 
 
 encoder : Guarded i a e -> a -> Value
+encoder (Guarded _ _ encoderI _ unboxFn) val =
+    unboxFn val
+        |> encoderI
 
 
 
@@ -29,9 +50,21 @@ type IntError
 
 
 gt : Int -> Int -> Result IntError Int
+gt bound val =
+    if bound <= val then
+        Err BelowRange
+
+    else
+        Ok val
 
 
 lt : Int -> Int -> Result IntError Int
+lt bound val =
+    if bound >= val then
+        Err AboveRange
+
+    else
+        Ok val
 
 
 
@@ -45,9 +78,32 @@ type StringError
 
 
 minLength : Int -> String -> Result StringError String
+minLength bound val =
+    if String.length val < bound then
+        Err TooShort
+
+    else
+        Ok val
 
 
 maxLength : Int -> String -> Result StringError String
+maxLength bound val =
+    if String.length val > bound then
+        Err TooLong
+
+    else
+        Ok val
 
 
 regexMatch : String -> String -> Result StringError String
+regexMatch pattern val =
+    let
+        regex =
+            Regex.fromString pattern
+                |> Maybe.withDefault Regex.never
+    in
+    if Regex.contains regex val then
+        Ok val
+
+    else
+        Err NotMatchingRegex
