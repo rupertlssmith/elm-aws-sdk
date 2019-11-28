5fdd6fc83e4b76773beddfebf5677451f5fdf4bc
Rupert Smith <rupert@thesett.com>
2019-11-28 11:58:28 +0000

    Implemented the AWS call.

--- api/lambda/src/Routing/API.elm
+++ api/lambda/src/Routing/API.elm
@@ -1,10 +1,18 @@
 port module Routing.API exposing (Conn, Route(..), main, requestPort, responsePort, router)
 
-import AWS.CognitoIdentityProvider
+import AWS.CognitoIdentityProvider as CIP
+import AWS.Core.Credentials
+import AWS.Core.Decode
+import AWS.Core.Http
+import Http exposing (Error(..))
 import Json.Encode as Encode
+import Refined
 import Serverless
 import Serverless.Conn exposing (jsonBody, method, respond, route, textBody)
 import Serverless.Conn.Request exposing (Method(..))
+import Serverless.Conn.Response exposing (Response, Status)
+import Task
+import Task.Extra
 import Url
 import Url.Parser exposing ((</>), map, oneOf, s, string, top)
 
@@ -63,9 +71,41 @@ router conn =
             respond ( 405, textBody "Method not allowed" ) conn
 
 
+
+-- AWS Services
+
+
+credentials =
+    AWS.Core.Credentials.fromAccessKeys
+        "keyId"
+        "secret"
+
+
 listUserPools : Cmd Msg
 listUserPools =
-    Cmd.none
+    let
+        nextTokenRes =
+            Refined.build CIP.paginationKeyType "blah"
+                |> Result.mapError (Refined.errorToString CIP.paginationKeyType)
+
+        maxResultsRes =
+            Refined.build CIP.poolQueryLimitType 20
+                |> Result.mapError (Refined.errorToString CIP.poolQueryLimitType)
+    in
+    case ( nextTokenRes, maxResultsRes ) of
+        ( Ok nextToken, Ok maxResults ) ->
+            CIP.listUserPools
+                { nextToken = nextToken
+                , maxResults = maxResults
+                }
+                |> AWS.Core.Http.send CIP.service credentials
+                |> Task.attempt ListUserPoolsResponse
+
+        ( Err err, _ ) ->
+            Error err |> Task.Extra.message
+
+        ( _, Err err ) ->
+            Error err |> Task.Extra.message
 
 
 
@@ -73,11 +113,43 @@ listUserPools =
 
 
 type Msg
-    = ListUserPoolsResponse
+    = ListUserPoolsResponse (Result Http.Error (AWS.Core.Decode.ResponseWrapper CIP.ListUserPoolsResponse))
+    | Error String
 
 
 update : Msg -> Conn -> ( Conn, Cmd Msg )
 update msg conn =
     case msg of
-        ListUserPoolsResponse ->
-            respond ( 200, jsonBody <| Encode.string "userPools" ) conn
+        ListUserPoolsResponse res ->
+            case res of
+                Err err ->
+                    respond (httpErrorToResponse err) conn
+
+                Ok wrapper ->
+                    respond ( 200, jsonBody <| Encode.string "userPools" ) conn
+
+        Error err ->
+            respond ( 500, textBody err ) conn
+
+
+
+-- HTTP Helpers
+
+
+httpErrorToResponse : Error -> ( Status, Response )
+httpErrorToResponse err =
+    case err of
+        BadUrl url ->
+            ( 500, textBody "Bad URL" )
+
+        Timeout ->
+            ( 504, textBody "Timeout" )
+
+        NetworkError ->
+            ( 500, textBody "Network Error" )
+
+        BadStatus status ->
+            ( status, textBody "Bad Status" )
+
+        BadBody val ->
+            ( 500, textBody val )
